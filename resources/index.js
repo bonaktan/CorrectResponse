988aaa481a212362f2c0011df781a8d0d1aa7a49
bonaktan <128207489+bonaktan@users.noreply.github.com>
2023-09-20 22:25:05 +0800

    Fixed the id=DIsplay?

--- resources/index.js
+++ resources/index.js
@@ -1,11 +1,15 @@
-var DisplayCells = '<div>?.</div><div id="Display?"></div>'
 $( document ).ready(function() {
   $('.Count').hide()
   // TODO: Adjustable Counter
+  const template = ['<div>', "", '.</div><div id="Display', "", '"></div>']
   for (i=1; i<=25; i++) {
-    $(".Display").append(DisplayCells.replace("?", i))
-    $(".Display").append(DisplayCells.replace("?", i+25))
-    document.querySelector("#Display1").innerHTML = "0"
+    var cache = template;
+    template[1] = i; template[3] = i;
+    $(".Display").append(template.join(''));
+    template[1] = i+25; template[3] = i+25;
+    $(".Display").append(template.join(''));
+    document.querySelector("#Display"+i).innerHTML = "0"; j = i+25
+    document.querySelector("#Display"+j).innerHTML = "0"; i 
 
   }
 });
