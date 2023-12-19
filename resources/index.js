52ab05518515c56121cd35556978831a5cda6245
bonaktan <128207489+bonaktan@users.noreply.github.com>
2023-12-19 21:46:50 +0800

    I HATE JQUERY

--- resources/index.js
+++ resources/index.js
@@ -1,25 +1,7 @@
-$(document).ready(function() {
-
-  // Global Constants/Variables
-  const inputregex = /(^[0-9]+$)|((^[0-9]+)-([0-9]+$))/g;
-  const template = ['<div>', "", '.</div><div id="Display', "", '"></div>'];
-  var items = Math.floor(24/2); /* TODO: Modifiable at ui level */;
-
-
-  $('.Count').hide();
-  for (i=1; i<=items; i++) {
-    var cache = template;
-    cache[1] = i; cache[3] = i;
-    $(".Display").append(cache.join(''));
-    cache[1] = i+items; cache[3] = i+items;
-    $(".Display").append(cache.join(''));
-    document.querySelector("#Display"+i).innerHTML = "0"; j = i+items;
-    document.querySelector("#Display"+j).innerHTML = "0";
-  }
-
-  $("#NumberSubmit").click(function() {
-     
-  });
-});
-
+// Global Constants/Variables
+const displayTemplate = ['<div>', "", '.</div><div id="Display', "", '"></div>'];
+const inputRegex = /^\d+$/g;
+var items = 0;
+var itemsRender = Math.floor(items/2); /* BUG: Issues whenever odd yung divident */;
+var NumberArray = new Array(items);
 
