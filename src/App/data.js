750cf7d0e7530af0e0df06bf9ec0047ccccdd007
bonaktan <128207489+bonaktan@users.noreply.github.com>
2024-01-21 21:18:52 +0800

    CLEANER CODE YES FINALLY

--- src/App/data.js
+++ src/App/data.js
@@ -1,14 +1,13 @@
-import {useState} from "react"
+import {useRef} from "react"
 
 const Counter = (items) => {
-    const [values, setValues] = useState(new Array(items).fill(0))
-
+    const values = useRef(new Array(items).fill(0))
+    const page = useRef({ prev: false, next: true, num: 1, max: values.current.length, value: values.current[0]})
     const submitValue = (num) => { 
-        let temp = values
-        temp[num-1] += 1
-        setValues(temp) 
+        values.current[num-1] += 1
+        console.log(values)
     }
-    return {values, submitValue}
+    return {values, submitValue, page}
 } 
 
 export default Counter;
--- src/App/ui.jsx
+++ src/App/ui.jsx
@@ -2,27 +2,41 @@ import useInput from "./main.js"
 
 const CorrectResponser = () => {
     const { Mode, values,
-            toggleMode, submitValue } = useInput()
+            toggleMode,
+            submitValue, sanityCheck, error,
+            page, pagenav,} = useInput()
     const Navbar = () => { return (
         <>
-            <p>Correct Responser</p>
-            <button onClick={toggleMode}>Swap</button>
+            <div>
+                <p>Correct Responser</p>
+                <button onClick={toggleMode}>Swap</button>
+            </div>
         </>
     )}
 
     const Table = () => { return (
         <>
             <form onSubmit={submitValue}>
-                <input></input>
+                <input onChange={sanityCheck} onInvalid={error}></input>
                 <button type="submit">Submit</button>
             </form>
-            <p>{values}</p>
+            <div>{values}</div>
         </>
 
     )}
 
-    const Display = () => { return (
-        <p>Display</p>
+    const Display = () => { 
+        return (
+        <>
+            <span>
+                <button onClick={() => pagenav("PMAX")} disabled={!page.prev}>&lt;&lt;</button>
+                <button onClick={() => pagenav("PONE")} disabled={!page.prev}>&lt;</button>
+                <p>{page.num}/{page.max}</p>
+                <button onClick={() => pagenav("NONE")} disabled={!page.next}>&gt;</button>
+                <button onClick={() => pagenav("NMAX")} disabled={!page.next}>&gt;&gt;</button>
+            </span>
+            <p>{page.value}</p>
+        </>
     )}
 
     return (
