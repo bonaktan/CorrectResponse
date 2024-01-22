4d0593f14776c7754b350726aedb04d325d54ea2
bonaktan <128207489+bonaktan@users.noreply.github.com>
2024-01-22 17:47:15 +0800

    Revert "ui rework 1: im van gagogh"
    This reverts commit 2465aace0ea69f7b26927997a10f7228a3ad91a7.

--- src/App/ui.jsx
+++ src/App/ui.jsx
@@ -1,44 +1,24 @@
-// Globals
-import { createTheme, ThemeProvider, ToggleButton, Button, FormControl, FormLabel, TextField } from "@mui/material"
-import CssBaseline from "@mui/material/CssBaseline";
-import Stack from '@mui/material/Stack';
-import { grey } from "@mui/material/colors";
-
-import SettingsIcon from '@mui/icons-material/Settings';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-// Locals
 import useInput from "./main.js"
 
 const CorrectResponser = () => {
     const { Mode, values,
             toggleMode,
-            submitValue, sanityCheck,
+            submitValue, sanityCheck, error,
             page, pagenav,} = useInput()
-    const theme = createTheme({
-        palette: {
-            background: { default: grey[900]},
-            primary: grey,
-            text: { primary: "#fff" }
-        }
-    })
-
     const Navbar = () => { return (
-        <Stack direction="row" alignItems="baseline" justifyContent="space-around" sx={{borderBottom: 1, borderColor: "primary"}}>
-            <p>Correct Responser</p>
-            <Stack direction="row" justifyContent="space-evenly" >
-                <Button><SettingsIcon/></Button>
-                <ToggleButton value="ModeChange" selected={Mode} onChange={toggleMode} sx={{color: "primary"}}><VisibilityIcon/></ToggleButton>
-            </Stack>
-        </Stack>
+        <>
+            <div>
+                <p>Correct Responser</p>
+                <button onClick={toggleMode}>Swap</button>
+            </div>
+        </>
     )}
 
     const Table = () => { return (
         <>
             <form onSubmit={submitValue}>
-                <TextField variant="standard" label="Input" size="small" onChange={sanityCheck}></TextField> {/* TODO: Error Display */}
-                <Button type="submit">Submit</Button>
-                {/* <input onChange={sanityCheck} onInvalid={error}></input>
-                <button type="submit">Submit</button> */ }
+                <input onChange={sanityCheck} onInvalid={error}></input>
+                <button type="submit">Submit</button>
             </form>
             <div>{values}</div>
         </>
@@ -60,11 +40,10 @@ const CorrectResponser = () => {
     )}
 
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
+        <>
             <Navbar/>
             { ( Mode ) ? <Display/> : <Table/> }
-        </ThemeProvider>
+        </>
     )
 }
 
