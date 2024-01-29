e1f703ff30b70ec5fa46a30f6b8f486436836ea7
bonaktan <128207489+bonaktan@users.noreply.github.com>
2024-01-29 11:39:05 +0800

    Subtract Mode Implemented on Backend

--- src/App/data.js
+++ src/App/data.js
@@ -10,7 +10,7 @@ const Counter = (items) => {
         max: values.current.length,
         value: values.current[0],
     };
-    const [InputMode, setInputMode] = useState(true); // true = Add, false = Subtract
+    const [InputMode, setInputMode] = useState(false); // true = Add, false = Subtract
     const commands = useRef([]);
     const AddModeSubmit = (input) => {
         // this thing has been sanitychecked already
@@ -59,7 +59,7 @@ const Counter = (items) => {
         }
     };
 
-    const AddSanityCheck = (e) => {
+    const AddSanityCheck = (e) => { // theres still prolly a chance that we can merge add and subtract modes
         let validity = "";
         let input = e.target.value;
         let isSubtract = false;
@@ -84,7 +84,7 @@ const Counter = (items) => {
         }
         input = input.split("-").map((val) => parseInt(val));
         if (input.length === 2) {
-            // Ranged Jalues
+            // Ranged Values
             if (!(input[0] < input[1])) {
                 validity = "First Value should be less than Second Value";
             } else if (!(1 <= input[0] && input[0] <= DisplayData.max)) {
@@ -109,8 +109,55 @@ const Counter = (items) => {
         e.target.setCustomValidity(validity);
     };
     const SubtractSanityCheck = (e) => {
-        alert("Not Implemented");
+        let validity = "";
+        let input = e.target.value;
+        let isAdd = false;
+        // maybe go for a full regex on this one
+        if (input[0] === "-") {
+            isAdd = true;
+            input = input.substring(1);
+        }
+
+        if (!/(^\.$)|(^\d+(-\d+$)?$)/g.test(input)) {
+            e.target.setCustomValidity("Invalid Input");
+            return; // efficiency
+        }
+
+        if (input === ".") {
+            console.log(commands.current);
+            if (commands.current.length <= 0) {
+                validity = "No Undo Available";
+            }
+            e.target.setCustomValidity(validity);
+            return;
+        }
+        input = input.split("-").map((val) => parseInt(val));
+        if (input.length === 2) {
+            // Ranged Values
+            if (!(input[0] < input[1])) {
+                validity = "First Value should be less than Second Value";
+            } else if (!(1 <= input[0] && input[0] <= DisplayData.max)) {
+                validity = "First Value Out of Range";
+            } else if (!(1 <= input[1] && input[1] <= DisplayData.max)) {
+                validity = "Second Value Out of Range";
+            } else if (
+                !isAdd &&
+                values.current
+                    .slice(input[0] - 1, input[1] - 1)
+                    .some((n) => n <= 0)
+            ) {
+                validity = "Atleast 1 number is already 0";
+            }
+        } else {
+            if (!(1 <= input[0] && input[0] <= DisplayData.max)) {
+                validity = "Out of Range";
+            } else if (!isAdd && values.current[input[0] - 1] <= 0) {
+                validity = "Number is already 0";
+            }
+        }
+        e.target.setCustomValidity(validity);
     };
+
 
     const setDisplayData = (state, num) => {
         state.num = num;
